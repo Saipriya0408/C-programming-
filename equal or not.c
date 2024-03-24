266a2d06045eb2f9e0d72296a005ed650d66f14e
Saipriya0408 <161060568+Saipriya0408@users.noreply.github.com>
2024-03-24 14:38:06 +0530

    Add files via upload

---
+++ equal or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=5,b=9;
+	if(a==b)
+	{
+		printf("the given numbers are equal");
+	}
+	else
+	{
+		printf("the given numbers are not equal");
+	}
+	return 0;
+}
