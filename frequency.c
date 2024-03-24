a9c5892efb2269b3d497ccaffd0a349d288a3c5f
Saipriya0408 <161060568+Saipriya0408@users.noreply.github.com>
2024-03-24 14:42:35 +0530

    Add files via upload

--- frequency.c
+++ frequency.c
@@ -1,19 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int num[]={1,2,3,3,4,5,6,7,8,9};
-	int len=sizeof(num)/sizeof(num[0]);
-	int freq[256]={0},i;
-	for(i=0;i<len;i++)
+	char str[1000]="saveeeeeetha",ch='e',i;
+	int count=0;
+	for(i=0;str[i]!=0;i++)
 	{
-		freq[num[i]]++;
-	}
-	for(i=0;i<256;i++)
-	{
-		if(freq[i]>0)
-		{
-			printf("the frequency of %d is %d\n",i,freq[i]);
-		}
-	}
+		if(ch==str[i])
+		count++;
+	} 
+	printf("frequency =%d",count);
 	return 0;
 }
