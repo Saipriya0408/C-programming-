e7ae493c9039e848310234a8f4b086206cc407b6
Saipriya0408 <161060568+Saipriya0408@users.noreply.github.com>
2024-03-24 14:34:31 +0530

    Add files via upload

---
+++ bankbalance.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int kumar=200,withdraw=500;
+	if(500<=200)
+	{
+		printf("you can get the money");
+		
+    }
+    else
+    {
+    	printf("insufficient balance");
+	}
+	return 0;
+}
